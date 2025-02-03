a9d1d79a96f6a3da37333bc327aa91572c4d4775
Reina <notmarx.tech@gmail.com>
2025-02-03 14:05:44 +0700

    feat(db): change file name

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chromoe_db::driver::sqlite_driver::SQLiteDriver;
+use chromoe_db::structure::SQLiteDriverOptions;
 
 mod args;
 mod cli;
@@ -7,7 +8,11 @@ mod subcommands;
 
 fn main() {
     let matches = cli::build_cli();
-    let driver = SQLiteDriver::new(None).unwrap();
+    let driver_options = SQLiteDriverOptions {
+        file_name: "kou_db.sqlite".to_string(),
+        table_name: "kou_db".to_string(),
+    };
+    let driver = SQLiteDriver::new(Some(driver_options)).unwrap();
 
     cli::handle_matches(&matches, driver);
 }
